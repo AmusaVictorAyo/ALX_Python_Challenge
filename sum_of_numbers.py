3170913f4e517400b66a129f5c0876f7479dd765
AmusaVictorAyo <victoryamusaatlast@gmail.com>
2026-02-23 07:04:49 -0500

    Using for loop to add numbers from 1 to 10

---
+++ sum_of_numbers.py
@@ -0,0 +1,6 @@
+numbers  = [1,2,3,4,5,6,7,8,9,10] # List of numbers from 1 to 10
+total  = 0 
+
+for each in numbers: # Loop through each number in the list and add it to total
+    total += each
+print(f"The sum of numbers in list are {total}") # Print the total sum of the numbers in the list
